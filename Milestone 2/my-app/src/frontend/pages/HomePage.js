165b3750ac12b73ef1af59126ecee1996d126ccc
valpal <svalery6@gmail.com>
2024-10-01 15:04:08 -0700

    added buttons

--- Milestone 2/my-app/src/frontend/pages/HomePage.js
+++ Milestone 2/my-app/src/frontend/pages/HomePage.js
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom';
 
 import '../styles/HomePage.css';
 import Button from '@mui/material/Button';
+import Logo from '../assets/sengLogo.jpg';
 
 const HomePage = ({user}) => {
     
     return(
         <div className='landing-page'>
             <h1 style ={{justifyContent: 'center', color: 'grey', textAlign: 'center', fontWeight: 'bold', fontSize: '5em'}}> WELCOME TO MISTER ED
-            </h1>
+            </h1> 
+            <img src={Logo}></img>
             <div className='action-selection'>
-            <Button component={Link} to={`/Profile`} style={{color: 'white', justifyContent: 'center', background:'#597D35', border: '2px solid black', width: '10em', alignContent:'center'}}>Check Profile</Button>
+            <Button component={Link} to={`/Profile`} style={{color: 'white', justifyContent: 'center', background:'#597D35', border: '2px solid black', width: '10em', alignContent:'center', margin:'5px'}}>Profile</Button>
+            <Button component={Link} to={`/Times`} style={{color: 'white', justifyContent: 'center', background:'#597D35', border: '2px solid black', width: '10em', alignContent:'center', margin:'5px'}}>ED Times</Button>
+            <Button component={Link} to={`/Profile`} style={{color: 'white', justifyContent: 'center', background:'#990F02', border: '2px solid black', width: '10em', alignContent:'center', margin:'5px'}}>Notifications</Button>
             </div>
         </div>
     );
