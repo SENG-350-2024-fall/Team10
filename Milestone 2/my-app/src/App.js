c51b3c50f45ccfd2692a0d56cdc3ec21a0f16eab
valpal <svalery6@gmail.com>
2024-10-01 14:00:48 -0700

    initial react site set up

---
+++ Milestone 2/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+
+    return (
+        <Router>
+            <Routes>
+            <Route path="/" />  {/* the landing home page*/}
+            </Routes>
+        </Router>
+    );
+};
+export default App;
