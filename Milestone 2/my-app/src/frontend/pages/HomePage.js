c25613730afaeb4f4f280f3e433964a24f594390
valpal <svalery6@gmail.com>
2024-10-01 14:57:09 -0700

    updates styles

--- Milestone 2/my-app/src/frontend/pages/HomePage.js
+++ Milestone 2/my-app/src/frontend/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+
+import '../styles/HomePage.css';
 import Button from '@mui/material/Button';
 
 const HomePage = ({user}) => {
@@ -9,7 +11,7 @@ const HomePage = ({user}) => {
         <div className='landing-page'>
             <h1 style ={{justifyContent: 'center', color: 'grey', textAlign: 'center', fontWeight: 'bold', fontSize: '5em'}}> WELCOME TO MISTER ED
             </h1>
-            <div className='action-selction'>
+            <div className='action-selection'>
             <Button component={Link} to={`/Profile`} style={{color: 'white', justifyContent: 'center', background:'#597D35', border: '2px solid black', width: '10em', alignContent:'center'}}>Check Profile</Button>
             </div>
         </div>
