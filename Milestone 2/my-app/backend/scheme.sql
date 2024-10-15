129434abd4d8552524c31c2f55c6e25a98164f06
valpal <svalery6@gmail.com>
2024-10-15 16:15:37 -0700

    schema up

--- Milestone 2/my-app/backend/scheme.sql
+++ Milestone 2/my-app/backend/scheme.sql
@@ -3,9 +3,9 @@ CREATE DATABASE ED;
 USE ED;
 
 CREATE TABLE Patients (
-    healthcarenumber VARCHAR(20) PRIMARY KEY,  -- Primary Key: healthcare number
-    name VARCHAR(100) NOT NULL,                -- Name of the patient
-    age INT NOT NULL,                          -- Age of the patient
-    phone_number VARCHAR(15),                   -- Phone number of the patient
-    profile_image VARCHAR(255),
+    healthcarenumber VARCHAR(20) PRIMARY KEY,  
+    name VARCHAR(100) NOT NULL,                
+    age INT NOT NULL,                          
+    phone_number VARCHAR(15),                  
+    profile_image VARCHAR(255)
 );
